1afa2c3a4a8732c1368c9c17bfc646bfa86b7789
alex shivers <alex.shivers.gith@gmail.com>
2025-11-19 18:33:19 +0300

    complete splitting components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer className="app-footer">
+        <p>© 2025 UI Component Library. All rights reserved.</p>
+        <div className="technologies-used">
+          <strong>Technologies used:</strong> React, JSX, useState, Conditional Rendering, CSS
+          Modules, Event Handling, Component Composition, Reusable Components.
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
